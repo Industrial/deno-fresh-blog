938142f03a5af9a7052df1a9f003bf25944b0a55
Tom Wieland <tom.wieland@gmail.com>
2022-09-11 17:15:46 +0200

    feature(app): blue navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
 export function Navbar() {
   return (
-    <div className="flex justify-center items-center h-20 bg-red-600 text-white">
-      <a className="p-2" href="/">Home</a>
-      <a className="p-2" href="/about">About</a>
-      <a className="p-2" href="/contact">Contact</a>
-    </div>
+    <>
+      <div className="flex justify-center items-center h-20 bg-white text-black">
+        <a className="pl-4 pr-4 pt-7 pb-7" href="/">Home</a>
+        <a className="pl-4 pr-4 pt-7 pb-7" href="/about">About</a>
+        <a className="pl-4 pr-4 pt-7 pb-7" href="/contact">Contact</a>
+      </div>
+      <div className="flex justify-center items-center h-20 bg-blue-400 font-semibold text-white text-5xl">
+        <h1>#Code9</h1>
+      </div>
+    </>
   );
 }
